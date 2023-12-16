ace5b47b72ae0bea95ed56ec23cc388504ea7fda
Parvez Ahmed Prottoy <parvezahmedprottoy.web@gmail.com>
2023-12-16 12:59:57 +0600

    added navigation section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const hamMenu = document.querySelector(".bx-menu-alt-right");
+const mobileNav = document.querySelector(".mobile-nav");
+hamMenu.addEventListener("click", () => {
+  mobileNav.classList.toggle("mobile-nav-open");
+  hamMenu.classList.toggle("bx-x");
+});
